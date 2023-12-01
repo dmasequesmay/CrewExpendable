ed11a0fe0555782e33c53f286f98817d50f11ee4
William <WilliamMTriDo@gmail.com>
2023-12-01 13:34:18 -0800

    Removed attack functions from .h files

--- header/Alien.h
+++ header/Alien.h
@@ -12,7 +12,6 @@ class Alien : public Character {
     protected:
         AlienType alienClass;
     public:
-        virtual void attack(Character &) = 0;
 }
 
 #endif
--- header/AlienType.h
+++ header/AlienType.h
@@ -10,17 +10,13 @@ using namespace std;
 class TankAlien : public Player {
     public: 
         tankAlien(const string &name, double health, double stamina, double attackDamage);
-        void attack(Character &) override;
 }
 
 class SlipperyAlien : public Player {
     public: 
         slipperyAlien(const string &name, double health, double stamina, double attackDamage);
-        void attack(Character &) override;
 }
 
 class BossAlien : public Player {
     public: 
         bossAlien(const string &name, double health, double stamina, double attackDamage);
-        void attack(Character &) override;
-}
