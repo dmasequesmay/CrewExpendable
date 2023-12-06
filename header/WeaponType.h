7725218d576bebbe2878e77d64b3bd28e0e670ca
William <WilliamMTriDo@gmail.com>
2023-12-05 22:19:54 -0800

    Fixed Errors

--- header/WeaponType.h
+++ header/WeaponType.h
@@ -2,21 +2,22 @@
 #define WEAPONTYPE_H
 #include <string>
 
-#include "header/Weapon.h"
+#include "../header/Weapon.h"
 using namespace std;
 
 class HeavyWeapon : public Weapon {
     public: 
-        heavyWeapon(WeaponType type, string name, double attackDamage, double criticalChance);
+        HeavyWeapon(WeaponType type, string name, double attackDamage, double criticalChance);
 };
 
 class MediumWeapon : public Weapon {
     public:
-        mediumWeapon(WeaponType type, string name, double attackDamage, double criticalChance);
-}
+        MediumWeapon(WeaponType type, string name, double attackDamage, double criticalChance);
+};
+
 class LightWeapon : public Weapon {
     public: 
-        lightWeapon(WeaponType type, string name, double attackDamage, double criticalChance);
+        LightWeapon(WeaponType type, string name, double attackDamage, double criticalChance);
 };
 
 #endif
--- src/WeaponType.cpp
+++ src/WeaponType.cpp
@@ -1,22 +1,13 @@
 #include "../header/WeaponType.h"
 #include <iostream>
 
-HeavyWeapon::heavyWeapon(WeaponType type, string name, double attackDamage, double criticalChance)
+HeavyWeapon::HeavyWeapon(WeaponType type, string name, double attackDamage, double criticalChance)
     :Weapon(WeaponType type, string name, double attackDamage, double criticalChance)
-{
-    type = HeavyWeapon;
-    attackDamage = 20.0
-    criticalChance = 15;
-    durability = 50;
-    staminaCost = 20;
-}
+{}
 
-LightWeapon::lightWeapon(WeaponType type, string name, double attackDamage, double criticalChance)
+MediumWeapon::MediumWeapon(WeaponType type, string name, double attackDamage, double criticalChance)
     :Weapon(WeaponType type, string name, double attackDamage, double criticalChance)
-{
-    type = LightWeapon;
-    attackDamage = 10.0
-    criticalChance = 45;
-    durability = 30;
-    staminaCost = 10;
-}
+
+LightWeapon::LightWeapon(WeaponType type, string name, double attackDamage, double criticalChance)
+    :Weapon(WeaponType type, string name, double attackDamage, double criticalChance)
+{}
