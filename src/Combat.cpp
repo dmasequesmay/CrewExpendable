1223c1dd2091724ad7c3f6220a7d848a7de60c83
William <WilliamMTriDo@gmail.com>
2023-12-05 23:00:25 -0800

    Implemented critical chance

--- src/Combat.cpp
+++ src/Combat.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <time.h>
 #include "../header/Combat.h"
 
 void Combat::attack(Character &attacker, Character &attackee) {
+    srand(time(0));
     double damage = attacker.getAttackDamage();
+    int crit = 1 + (rand() % 100); // rolls for critical chance
+    if (crit <= 15) {
+        damage *= 1.2;
+    }
     attackee.damage(damage);
 }
 
