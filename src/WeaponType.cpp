46e5c93eece8e84c13d7f08e257e4f6c9cd3f236
dmasequesmay <dominique.masequesmay@gmail.com>
2023-12-06 21:20:50 -0800

    Add MediumWeapon

--- src/WeaponType.cpp
+++ src/WeaponType.cpp
@@ -5,6 +5,10 @@ HeavyWeapon::HeavyWeapon(string name)
     :Weapon(HEAVYWEAPON, name, 20.0, 15)
 {}
 
+MediumWeapon::MediumWeapon(string name)
+    :Weapon(MEDIUMWEAPON, name, 15.0, 30)
+{}
+
 LightWeapon::LightWeapon(string name)
     :Weapon(LIGHTWEAPON, name, 10.0, 45)
 {}
