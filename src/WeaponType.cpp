9064eba91692d5151f79cb7e81de0632d7b355a9
dmasequesmay <dominique.masequesmay@gmail.com>
2023-12-06 20:21:41 -0800

    Fixes issues #29 and #30

--- src/WeaponType.cpp
+++ src/WeaponType.cpp
@@ -1,18 +1,10 @@
 #include "../header/WeaponType.h"
 #include <iostream>
 
-HeavyWeapon::HeavyWeapon(string name, double attackDamage, double criticalChance)
-    :Weapon(HEAVYWEAPON, name, attackDamage, criticalChance)
-{
-    // wType = HeavyWeapon;
-    // attackDamage = 20.0
-    // criticalChance = 15;
-}
+HeavyWeapon::HeavyWeapon(string name)
+    :Weapon(HEAVYWEAPON, name, 20.0, 15)
+{}
 
-LightWeapon::LightWeapon(string name, double attackDamage, double criticalChance)
-    :Weapon(LIGHTWEAPON, name, attackDamage, criticalChance)
-{
-    // wType = LightWeapon;
-    // attackDamage = 10.0
-    // criticalChance = 45;
-}
+LightWeapon::LightWeapon(string name)
+    :Weapon(LIGHTWEAPON, name, 10.0, 45)
+{}
