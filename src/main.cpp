e935212b1c53e20237bd3f0d51a0ceef36b58407
William <WilliamMTriDo@gmail.com>
2023-12-07 19:23:29 -0800

    Implementing combat into main

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include "../header/Player.h"
 #include "../header/PlayerType.h"
 
+void Combat(Character &newPlayer, Character &newAlien);
+
 using namespace std;
 
 void wait() {
@@ -55,5 +57,62 @@ int main() {
     game.inputSelect(Tree);
     }
 
+    
+
     return 0;
+}
+
+void Combat(Character &newPlayer, Character &newAlien) {
+    int userChoice;
+    int itemChoice;
+    bool combatEnd = false;
+    while (!combatEnd) {
+        Combat newCombat;
+        cout << "------- It is your turn ------" << endl;
+        cout << "Choose your action:" << endl;
+        cout << "1.Attack 2.Block 3.Dodge 4.Items" << endl;
+        cin >> charChoice; 
+        if (charChoice == 1) {
+            newCombat.attack(newPlayer,newAlien);
+            if (!newAlien.isAlive()) {
+                combatEnd = true;
+            }
+            else {
+                cout << "------- It is the Alien's turn ------" << endl;
+                newCombat.attack(newAlien,newPlayer);
+            }
+        }
+        else if (charChoice == 2) {
+            newCombat.block(newAlien,newPlayer);
+        }
+        else if (charChoice == 3) {
+            newCombat.dodge(newAlien,newPlayer);
+        }
+        else {
+            newPlayer.getInventory();
+            cout << endl;
+            cout << "Which item would you like to use?" << endl;
+            cin >> itemChoice;
+            if (itemChoice == 1) {
+                cout << "Used a health potion, you heal 25.0 health" << endl;
+                newPlayer.getHeal();
+            }
+            else {
+                cout << "This item cannot be used." << endl;
+            }
+            
+        }
+        if (newPlayer.isAlive() && !newAlien.isAlive() || !newPlayer.isAlive()) {
+            combatEnd = true;
+        }
+    }
+    if (!newPlayer.isAlive()) {
+        cout << newPlayer.getName() << " was defeated." << endl;
+        cout << "======= GAME OVER ========";
+    }
+    else if (!newAlien.isAlive()) {
+        cout << newAlien.getName() << " was defeated." << endl;
+        cout << "======= YOU WIN! ========";
+    }
+
 }
